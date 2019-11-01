c28def6ddba0c8f51ac2a6820d861f6299c4a55c
niloco <niloco@umich.edu>
2019-11-01 14:17:25 -0400

    Added multiply test and fixed sub test
    The change I made to the sub test got clobbered in the merge, so I
    fixed it again.

--- test_rpn.py
+++ test_rpn.py
@@ -8,7 +8,9 @@ class TestBasics(unittest.TestCase):
     
     def test_subtract(self):
         self.assertEqual(1, rpn.calculate('3 2 -'))
-        self.assertEqual(-1, rpn.calculate('2 3 -'))
+    
+    def test_mul(self):
+        self.assertEqual(96, calculat(12 8 *))
 
     def test_badinput(self):
         with self.assertRaises(TypeError):
