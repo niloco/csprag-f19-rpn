404e276a3d5c3ecdd22b2feb7d8c3ecb2fb94274
niloco <niloco@umich.edu>
2019-11-01 21:21:03 -0400

    Added a test for division
    Division meaning integer division.

--- test_rpn.py
+++ test_rpn.py
@@ -12,6 +12,9 @@ class TestBasics(unittest.TestCase):
     def test_mul(self):
         self.assertEqual(96, rpn.calculate('12 8 *'))
 
+    def test_div(self):
+        self.assertEqual(5, rpn.calculate('17 3 /'))
+
     def test_badinput(self):
         with self.assertRaises(TypeError):
             rpn.calculate('1 2 3 +')
