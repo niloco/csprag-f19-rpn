6cc54069d4dbf63dda250e70122dfd8280780c94
niloco <niloco@umich.edu>
2019-11-05 18:17:00 -0500

    Changed dictionary interface
    Replaced single_arg_ops and dual_arg_ops with operations, a
    dictionary that contains (id, fxn) tuples. The id is an enum
    called fxn_type which describes the interface of the function,
    or how many/what the arguments are.

--- dicts.py
+++ dicts.py
@@ -1,15 +1,21 @@
 import operator
 import fxns
+from enum import Enum
 
-dual_arg_ops = {
-    '+':    operator.add,
-    '-':    operator.sub,
-    '*':    operator.mul,
-    '/':    operator.floordiv,
-    '%':    operator.mod,
-    '**':   operator.pow,
-}
+class fxn_type(Enum):
+    stack = 0
+    single_arg = 1
+    dual_arg = 2
 
-single_arg_ops = {
-    '!':    fxns.factorial,
+operations = {
+    '+':    (fxn_type.dual_arg, operator.add),
+    '-':    (fxn_type.dual_arg, operator.sub),
+    '*':    (fxn_type.dual_arg, operator.mul),
+    '/':    (fxn_type.dual_arg, operator.floordiv),
+    '%':    (fxn_type.dual_arg, operator.mod),
+    '**':   (fxn_type.dual_arg, operator.pow),
+
+    '!':    (fxn_type.single_arg, fxns.factorial),
+
+    'S':    (fxn_type.stack, fxns.sum),
 }
