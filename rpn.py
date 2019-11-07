4fba9086df9b95429e1a8f73875509ef894a5e7b
niloco <niloco@umich.edu>
2019-11-06 23:45:22 -0500

    Changed the interface of calculate() and some tests
    test_bad_input now tests invalid operators, invalid stack states
    (too few operands) and makes sure that stack state is preserved
    in the event of an error.
    
    This has all been implemented by adding another error check in
    calculate() for IndexError, which can (for now) only occur when
    popping from an empty stack, and only returning the modified stack
    if the command sequence was valid. I would prefer to handle this
    via another method but the reference semantics of python are
    alluding me at this juncture.

--- rpn.py
+++ rpn.py
@@ -2,30 +2,33 @@ import dicts
 
 def calculate(input, stack):
     result = 0
+    modified = stack.copy()
 
     for token in input.split():
         try:
             value = int(token)
-            stack.append(value)
+            modified.append(value)
         except ValueError:
             try:
                 op_type, func = dicts.operations[token]
 
                 if op_type == dicts.fxn_type.stack:
-                    result = func(stack)
+                    result = func(modified)
                 elif op_type == dicts.fxn_type.single_arg:
-                    arg = stack.pop()
+                    arg = modified.pop()
                     result = func(arg)
                 else:
-                    arg1 = stack.pop()
-                    arg0 = stack.pop()
+                    arg1 = modified.pop()
+                    arg0 = modified.pop()
                     result = func(arg0, arg1)
 
-                stack.append(result)
+                modified.append(result)
             except KeyError:
                 raise TypeError('invalid operator: ' + token)
-
-    return result
+            except IndexError:
+                raise TypeError('too few operands on stack for operator ' + token)
+    
+    return result, modified
 
 def main():
     stack = list()
@@ -41,7 +44,7 @@ def main():
             return
         else:
             try:
-                result = calculate(user_input, stack)
+                result, stack = calculate(user_input, stack)
                 print('result: ', result)
             except TypeError as e:
                 print(e)
--- test_rpn.py
+++ test_rpn.py
@@ -3,33 +3,45 @@ import rpn
 
 class TestBasics(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(2, rpn.calculate('1 1 +', list()))
+        result, _ = rpn.calculate('1 1 +', list())
+        self.assertEqual(2, result)
     
     def test_subtract(self):
-        self.assertEqual(1, rpn.calculate('3 2 -', list()))
+        result, _ = rpn.calculate('3 2 -', list())
+        self.assertEqual(1, result)
     
     def test_mul(self):
-        self.assertEqual(96, rpn.calculate('12 8 *', list()))
+        result, _ = rpn.calculate('12 8 *', list())
+        self.assertEqual(96, result)
 
     def test_div(self):
-        self.assertEqual(5, rpn.calculate('17 3 /', list()))
+        result, _ = rpn.calculate('17 3 /', list())
+        self.assertEqual(5, result)
 
     def test_modulo(self):
-        self.assertEqual(2, rpn.calculate('17 3 %', list()))
+        result, _ = rpn.calculate('17 3 %', list())
+        self.assertEqual(2, result)
 
     def test_power(self):
-        self.assertEqual(125, rpn.calculate('5 3 **', list()))
+        result, _ = rpn.calculate('5 3 **', list())
+        self.assertEqual(125, result)
 
     def test_factorial(self):
-        self.assertEqual(1, rpn.calculate('0 !', list()))
-        self.assertEqual(720, rpn.calculate('6 !', list()))
+        result, _ = rpn.calculate('0 !', list())
+        self.assertEqual(1, result)
+        result, _ = rpn.calculate('6 !', list())
+        self.assertEqual(720, result)
 
     def test_sum(self):
-        self.assertEqual(0, rpn.calculate('1 3 2 -6 S', list()))
+        result, _ = rpn.calculate('1 3 2 -6 S', list())
+        self.assertEqual(0, result)
 
     def test_bad_input(self):
-        stack = [1, 2, 3, 4]
+        stack = [1, 2, 3, 4, 5, 3342]
         self.assertRaises(  TypeError, 
                             rpn.calculate, 
                             '0 23 k', stack)
-        self.assertEqual([1, 2, 3, 4], stack)
+        self.assertRaises(  TypeError,
+                            rpn.calculate,
+                            '1 +', list())
+        self.assertEqual([1, 2, 3, 4, 5, 3342], stack)
