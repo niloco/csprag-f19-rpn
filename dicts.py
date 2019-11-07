c779467281252ff298900db11226191110df751a
niloco <niloco@umich.edu>
2019-11-06 19:19:21 -0500

    Added .travis.yml file
    Don't know if it will work yet. Also added some comments to dicts.py.

--- dicts.py
+++ dicts.py
@@ -8,14 +8,15 @@ class fxn_type(Enum):
     dual_arg = 2
 
 operations = {
+    # function(arg0, arg1)
     '+':    (fxn_type.dual_arg, operator.add),
     '-':    (fxn_type.dual_arg, operator.sub),
     '*':    (fxn_type.dual_arg, operator.mul),
     '/':    (fxn_type.dual_arg, operator.floordiv),
     '%':    (fxn_type.dual_arg, operator.mod),
     '**':   (fxn_type.dual_arg, operator.pow),
-
+    # function(arg)
     '!':    (fxn_type.single_arg, fxns.factorial),
-
+    # function(stack)
     'S':    (fxn_type.stack, fxns.sum),
 }
